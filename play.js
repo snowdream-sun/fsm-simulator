811770b6fdaf5847b70421a7dafafffcd0351646
snowdream-sun <33110024+snowdream-sun@users.noreply.github.com>
2018-04-06 00:15:33 +0800

    Update play.js

--- play.js
+++ play.js
@@ -1,12 +1,8 @@
-function initFSM() {
-    FSM._inputs = USER_INPUT.inputs.slice();
-    FSM._outputs = USER_INPUT.outputs.slice();
-    FSM._states = USER_INPUT.states.slice(); // array of state names
-    FSM._stateObj = {};
-    // collection of state objects with state names being the keys
-    FSM._states.forEach(function(state) {
-        FSM._stateObj[state] = new State(state);
+function setFSMTransition() {
+    USER_INPUT.transitionT.forEach(function(row) {
+        var cur = row[0];
+        var input = row[1];
+        var next = row[2];
+        FSM._stateObj[cur]._transition[input] = next;
     });
-    FSM._resetState = FSM._stateObj[USER_INPUT.resetState];
-    FSM._curState = FSM._resetState;
 }
