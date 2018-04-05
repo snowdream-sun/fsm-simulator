372c05a4925e20c127c69395d819a7a15f583350
snowdream-sun <33110024+snowdream-sun@users.noreply.github.com>
2018-04-06 00:13:34 +0800

    Update play.js

--- play.js
+++ play.js
@@ -1,18 +1,12 @@
-EG_MOORE_2.inputs = ["A"];
-EG_MOORE_2.outputs = ["Y"];
-EG_MOORE_2.states = ["S0", "S1", "S2"];
-EG_MOORE_2.resetState = "S0";
-EG_MOORE_2.transitionT = [
-    [  "S0",     "0",     "S1"  ],
-    [  "S0",     "1",     "S0"  ],
-    [  "S1",     "0",     "S1"  ],
-    [  "S1",     "1",     "S2"  ],
-    [  "S2",     "0",     "S1"  ],
-    [  "S2",     "1",     "S0"  ]
-];
-EG_MOORE_2.type = "Moore";
-EG_MOORE_2.outputT = [
-    [  "S0",     "0"  ],
-    [  "S1",     "0"  ],
-    [  "S2",     "1"  ]
-];
+function initFSM() {
+    FSM._inputs = USER_INPUT.inputs.slice();
+    FSM._outputs = USER_INPUT.outputs.slice();
+    FSM._states = USER_INPUT.states.slice(); // array of state names
+    FSM._stateObj = {};
+    // collection of state objects with state names being the keys
+    FSM._states.forEach(function(state) {
+        FSM._stateObj[state] = new State(state);
+    });
+    FSM._resetState = FSM._stateObj[USER_INPUT.resetState];
+    FSM._curState = FSM._resetState;
+}
