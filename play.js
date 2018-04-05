56ee39bc1a7fbdf3ac816ce78edfafbdfba35ec9
snowdream-sun <33110024+snowdream-sun@users.noreply.github.com>
2018-04-05 23:57:48 +0800

    Update play.js

--- play.js
+++ play.js
@@ -1,6 +1,18 @@
-// Start Page: load_input.js
-sessionStorage.setItem("userInput", JSON.stringify(EG_MOORE_1));
-// Function Page: backend.js
-var USER_INPUT =
-  Object.assign({}, JSON.parse(sessionStorage.getItem("userInput")));
-
+EG_MOORE_2.inputs = ["A"];
+EG_MOORE_2.outputs = ["Y"];
+EG_MOORE_2.states = ["S0", "S1", "S2"];
+EG_MOORE_2.resetState = "S0";
+EG_MOORE_2.transitionT = [
+    [  "S0",     "0",     "S1"  ],
+    [  "S0",     "1",     "S0"  ],
+    [  "S1",     "0",     "S1"  ],
+    [  "S1",     "1",     "S2"  ],
+    [  "S2",     "0",     "S1"  ],
+    [  "S2",     "1",     "S0"  ]
+];
+EG_MOORE_2.type = "Moore";
+EG_MOORE_2.outputT = [
+    [  "S0",     "0"  ],
+    [  "S1",     "0"  ],
+    [  "S2",     "1"  ]
+];
