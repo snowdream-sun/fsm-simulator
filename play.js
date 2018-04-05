31449acd4997dd5d0fc27346d31af3cbfd410444
snowdream-sun <33110024+snowdream-sun@users.noreply.github.com>
2018-04-06 00:16:44 +0800

    Update play.js

--- play.js
+++ play.js
@@ -1,8 +1,17 @@
-function setFSMTransition() {
-    USER_INPUT.transitionT.forEach(function(row) {
-        var cur = row[0];
-        var input = row[1];
-        var next = row[2];
-        FSM._stateObj[cur]._transition[input] = next;
-    });
+function setFSMOutput() {
+    FSM._type = USER_INPUT.type;
+    if (FSM._type == "Moore") {
+        USER_INPUT.outputT.forEach(function(row) {
+            var cur = row[0];
+            var out = row[1];
+            FSM._stateObj[cur]._outputs = out;
+        });
+    } else {
+        USER_INPUT.outputT.forEach(function(row) {
+            var cur = row[0];
+            var input = row[1];
+            var out = row[2];
+            FSM._stateObj[cur]._outputs[input] = out;
+        });
+    }
 }
